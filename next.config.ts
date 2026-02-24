8d86f0570255334f4c94dfd0e6677a27c21bf757
Chad Wright <justdeejays@gmail.com>
2026-02-24 16:07:38 +0200

    fix: update next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    minimumCacheTTL: 60 * 60 * 24 * 30,
+    deviceSizes: [425, 800, 1400],
+    imageSizes: [425, 800, 1400],
   },
 };
 
