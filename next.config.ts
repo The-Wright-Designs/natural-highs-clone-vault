37acfe53dd0d62144c992b128ff1bbb94154cf69
Chad Wright <justdeejays@gmail.com>
2026-02-24 08:59:17 +0200

    fix: remove image cache policy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    minimumCacheTTL: 60 * 60 * 24 * 30,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
