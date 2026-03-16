f8716b9771a8121837449f5d471d62a0c69bb1c3
Chad Wright <justdeejays@gmail.com>
2026-03-16 11:48:46 +0200

    feat: add hlvd certificates to certain strains

--- app/strains/[strain]/page.tsx
+++ app/strains/[strain]/page.tsx
@@ -12,6 +12,8 @@ import { createStrainSlug } from "@/_lib/utils/slug-utils";
 import { generateProductSchema } from "@/_lib/seo-utils";
 
 import strainData from "@/_data/strains-data.json";
+import Link from "next/link";
+import Image from "next/image";
 
 interface StrainPageProps {
   params: Promise<{ strain: string }>;
@@ -85,47 +87,66 @@ const StrainPage = async ({ params, searchParams }: StrainPageProps) => {
         dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
       />
       <div className="max-w-[1280px] desktop:mx-auto px-5 desktop:px-10 py-15">
-      <div className="mb-10">
-        <ButtonLink href={buildBackUrl()} cssClasses="place-self-start">
-          Back to Strains
-        </ButtonLink>
-      </div>
-      <div className="flex flex-col gap-5 desktop:grid desktop:grid-cols-[480px_1fr] desktop:gap-x-10 desktop:gap-y-5">
-        <div className="flex w-full flex-col pb-5 border-b-4 border-green desktop:place-self-start">
-          <h2 className="text-heading">{strain.title}</h2>
-          <p
-            className="text-subheading font-light"
-            style={{ fontVariant: "small-caps" }}
-          >
-            {strain.supplier}
-          </p>
+        <div className="mb-10">
+          <ButtonLink href={buildBackUrl()} cssClasses="place-self-start">
+            Back to Strains
+          </ButtonLink>
         </div>
-        <div className="desktop:overflow-hidden desktop:order-first desktop:row-span-2">
-          <StrainSlider data={strain.images} />
-        </div>
-        <div className="flex flex-col gap-10 text-white">
-          <div className="flex flex-col gap-5">
-            <div className="flex justify-between items-center gap-5 desktop:justify-start desktop:gap-10">
-              <p className="text-heading">R{strain.price}</p>
-              <StockAvailabilityBadges inStock={strain.inStock} />
-            </div>
-
-            <StrainDescription description={strain.description} />
+        <div className="flex flex-col gap-5 desktop:grid desktop:grid-cols-[480px_1fr] desktop:gap-x-10 desktop:gap-y-5">
+          <div className="flex w-full flex-col pb-5 border-b-4 border-green desktop:place-self-start">
+            <h2 className="text-heading">{strain.title}</h2>
+            <p
+              className="text-subheading font-light"
+              style={{ fontVariant: "small-caps" }}
+            >
+              {strain.supplier}
+            </p>
           </div>
-          <StrainCartComponent
-            strainId={strainSlug}
-            strainName={strain.title}
-            strainPrice={strain.price}
-            strainImage={strain.images[0]}
-            inStock={strain.inStock}
-            cssClasses="desktop:justify-start"
+          <div className="desktop:overflow-hidden desktop:order-first desktop:row-span-2">
+            <StrainSlider data={strain.images} />
+          </div>
+          <div className="flex flex-col gap-10 text-white">
+            <div className="flex flex-col gap-5">
+              <div className="flex justify-between items-center gap-5 desktop:justify-start desktop:gap-10">
+                <p className="text-heading">R{strain.price}</p>
+                <StockAvailabilityBadges inStock={strain.inStock} />
+              </div>
+
+              <StrainDescription description={strain.description} />
+              {strain.certificate && (
+                <Link
+                  href={strain.certificate}
+                  aria-label={`${strain.title} Certificate of Analysis`}
+                  target="_blank"
+                  className="text-[18px] self-start p-2 -m-2 underline decoration-green underline-offset-4 desktop:p-0 desktop:m-0 desktop:hover:opacity-80 ease-in-out duration-300"
+                >
+                  <span className="text-[18px] font-semibold">HLVd</span>
+                  {"  "}
+                  Certificate
+                  <Image
+                    src="/icons/li_external-link.svg"
+                    alt="External Link Icon"
+                    width={18}
+                    height={18}
+                    className="inline-block ml-1 mb-0.5"
+                  />
+                </Link>
+              )}
+            </div>
+            <StrainCartComponent
+              strainId={strainSlug}
+              strainName={strain.title}
+              strainPrice={strain.price}
+              strainImage={strain.images[0]}
+              inStock={strain.inStock}
+              cssClasses="desktop:justify-start"
+            />
+          </div>
+          <StrainDetails
+            strain={strain}
+            cssClasses="grid gap-5 text-paragraph pt-10 mt-10 border-t-4 border-green desktop:col-span-2 desktop:gap-x-10 desktop:gap-y-5 desktop:grid-cols-[480px_1fr] desktop:mt-0 desktop:pt-0 desktop:border-t-0"
           />
         </div>
-        <StrainDetails
-          strain={strain}
-          cssClasses="grid gap-5 text-paragraph pt-10 mt-10 border-t-4 border-green desktop:col-span-2 desktop:gap-x-10 desktop:gap-y-5 desktop:grid-cols-[480px_1fr] desktop:mt-0 desktop:pt-0 desktop:border-t-0"
-        />
-      </div>
       </div>
     </>
   );
